dd6eaf07a9db8ea4e48cf342975f968b5964e5b4
SangPhan <sangkane1227@gmail.com>
2024-10-27 12:01:38 +0700

    bai tap

--- Basic/The del statement.py
+++
@@ -1,7 +0,0 @@
-a = [-3, 1234.5, 2.25, 123, 234, 5]
-del a[0]
-print(a)
-del a[2:4]
-print(a)
-del a[:]
-print(a)
--- Basic/thu7.py
+++
@@ -1,7 +0,0 @@
-n = int(input('nhap gia trị n: '))
-i=0
-sum=0
-while i<=2*n+1:
-    sum=2*i +1
-    i+=1
-print(sum)
